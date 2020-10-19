4d65dc087d659702818a8bedf8765ed96db9b101
SvenTUM <SvenTUM@users.noreply.github.com>
2020-10-19 11:05:41 +0200

    Add blog app

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    author = models.CharField(max_length=100)  # TODO: reference User Class
+    date_created = models.DateTimeField(auto_created=True, auto_now_add=True)
+    date_publish = models.DateTimeField()
