51f24113c9d62ff7bbe685206107a7686b14ce9a
SvenTUM <SvenTUM@users.noreply.github.com>
2020-10-20 15:36:44 +0200

    Register Models

--- yourvid/admin.py
+++ yourvid/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Category, Vote, Video, Comment
+
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
