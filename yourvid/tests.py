5f32270c126f2391828319d4c0646688589a8568
SvenTUM <SvenTUM@users.noreply.github.com>
2020-10-20 18:12:53 +0200

    Some more tests

--- yourvid/tests.py
+++ yourvid/tests.py
@@ -2,13 +2,13 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 # Create your tests here.
-from yourvid.models import Category, Video
+from yourvid.models import Category, Video, Vote
 
 
 class YourvidTestCase(TestCase):
-
     # Load Video as Fixture
     fixtures = ['sample2.json']
+
     # +1 User
     # +1 Video
     # +1 Category
@@ -25,6 +25,11 @@ class YourvidTestCase(TestCase):
         cat3 = Category.objects.create(name="Sport")
         cat4 = Category.objects.create(name="Übrige Kategorien")
 
+        # Create Votes
+        Vote.objects.create(user=u1, video=Video.objects.first(), score=1)
+        Vote.objects.create(user=u2, video=Video.objects.first(), score=1)
+        Vote.objects.create(user=u3, video=Video.objects.first(), score=1)
+
     def test_user_count(self):
         count = User.objects.count()
         self.assertEqual(count, 4)
@@ -38,3 +43,11 @@ class YourvidTestCase(TestCase):
         # Key +1 because of fictures
         self.assertEqual(obj.name, "Übrige Kategorien")
         self.assertEqual(obj.slug, "ubrige-kategorien")
+
+    def test_video_count(self):
+        count = Video.objects.count()
+        self.assertEqual(count, 1)
+
+    def test_vote_count(self):
+        count = Vote.objects.count()
+        self.assertEqual(count, 3)
