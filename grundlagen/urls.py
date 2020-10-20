094bd5a788c6cc7adceb6769844674059d6304db
SvenTUM <SvenTUM@users.noreply.github.com>
2020-10-20 14:13:27 +0200

    Adjust Settings for Media Files

--- grundlagen/urls.py
+++ grundlagen/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 import debug_toolbar
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from grundlagen import settings
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
     # path('videos/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
