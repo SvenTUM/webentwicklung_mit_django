0d214125f37be57e5b2292d1dc4310f6b9dc7b5b
SvenTUM <SvenTUM@users.noreply.github.com>
2020-10-23 11:31:54 +0200

    Add Create, Delete to BlogPost API

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -7,7 +7,3 @@ class BlogPostSerializer(serializers.ModelSerializer):
     class Meta:
         model = BlogPost
         fields = '__all__'
-
-        extra_kwargs = {
-            "url": {"view_name": "api:blogpost-detail", "lookup_field": "title"}
-        }
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,10 +1,13 @@
-from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import (
+    ListModelMixin, RetrieveModelMixin, UpdateModelMixin, CreateModelMixin, DestroyModelMixin
+)
 from rest_framework.viewsets import GenericViewSet
 
 from .serializers import BlogPostSerializer
 from ..models import BlogPost
 
 
-class BlogPostViewSet(RetrieveModelMixin, ListModelMixin, UpdateModelMixin, GenericViewSet):
+class BlogPostViewSet(CreateModelMixin, RetrieveModelMixin, ListModelMixin,
+                      UpdateModelMixin, DestroyModelMixin, GenericViewSet):
     serializer_class = BlogPostSerializer
     queryset = BlogPost.objects.all()
